e28bad0e963ec3dd000fe43929d42a2b987fcf77
Gustavo Santos <gustavo.heitor@outlook.com>
2022-02-11 19:11:20 -0300

    CheckersPiece capture and Man possibleCaptures

--- src/checkers/CheckersPiece.java
+++ src/checkers/CheckersPiece.java
@@ -1,14 +1,21 @@
 package checkers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import common.Color;
 import common.GamePosition;
 
-public class CheckersPiece extends Piece{
-	
+public class CheckersPiece extends Piece {
+
 	private Color color;
+	protected List<List<CheckersPiece>> capturedPieces;
+	protected List<List<Position>> capturePositions;
+	protected List<List<Position>> piecePositions;
+	protected int longestStreak;
 
 	public CheckersPiece(Board board, Color color) {
 		super(board);
@@ -18,13 +25,29 @@ public class CheckersPiece extends Piece{
 	public Color getColor() {
 		return color;
 	}
-	
+
 	public GamePosition getCheckersPosition() {
 		return GamePosition.fromPosition(position);
 	}
-	
+
+	public List<List<CheckersPiece>> getCapturedPieces() {
+		return capturedPieces;
+	}
+
+	public List<List<Position>> getCapturePositions() {
+		return capturePositions;
+	}
+
+	public List<List<Position>> getPiecePositions() {
+		return piecePositions;
+	}
+
+	public int getLongestStreak() {
+		return longestStreak;
+	}
+
 	protected boolean isThereOpponentPiece(Position position) {
-		CheckersPiece p = (CheckersPiece)getBoard().piece(position);
+		CheckersPiece p = (CheckersPiece) getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
 
@@ -33,13 +56,27 @@ public class CheckersPiece extends Piece{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	public boolean[][] possibleCaptures() {
-		return null;
+
+	public void capture() {
+		boolean[][] ghostPieces = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		this.capturedPieces = new ArrayList<>();
+		this.capturePositions = new ArrayList<>();
+		this.piecePositions = new ArrayList<>();
+		this.longestStreak = 0;
+		List<CheckersPiece> capturedPieces = new ArrayList<>();
+		List<Position> capturePositions = new ArrayList<>();
+		List<Position> piecePositions = new ArrayList<>();
+		int longestStreak = 0;
+		piecePositions.add(position);
+		possibleCaptures(ghostPieces, capturedPieces, piecePositions, capturePositions, longestStreak, position);
 	}
-	
+
+	public void possibleCaptures(boolean[][] ghostPieces, List<CheckersPiece> capturedPieces,
+			List<Position> capturePositions, List<Position> piecePositions, int longestStreak, Position position) {
+	}
+
 	public boolean isThereACapture() {
 		return false;
 	}
-	
+
 }
--- src/checkers/Man.java
+++ src/checkers/Man.java
@@ -1,10 +1,13 @@
 package checkers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Position;
 import common.Color;
 
-public class Man extends CheckersPiece{
+public class Man extends CheckersPiece {
 
 	public Man(Board board, Color color) {
 		super(board, color);
@@ -14,7 +17,7 @@ public class Man extends CheckersPiece{
 	public String toString() {
 		return "("; // to change
 	}
-	
+
 	public String getSecondLetter() {
 		return ")";
 	}
@@ -23,31 +26,177 @@ public class Man extends CheckersPiece{
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
-		Position p = new Position(0,0);
+		Position p = new Position(0, 0);
 		int move = 1;
 
 		if (getColor() == Color.WHITE) {
 			move = -1;
 		}
-		
-		p.setValues(position.getRow()+move, position.getColumn()+1);
+
+		p.setValues(position.getRow() + move, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-			mat[p.getRow()][p.getColumn()]=true;
+			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		p.setValues(position.getRow()+move, position.getColumn()-1);
+
+		p.setValues(position.getRow() + move, position.getColumn() - 1);
 		if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-			mat[p.getRow()][p.getColumn()]=true;
+			mat[p.getRow()][p.getColumn()] = true;
 		}
 
 		return mat;
 	}
-	
+
+	@Override
+	public void possibleCaptures(boolean[][] ghostPieces, List<CheckersPiece> capturedPieces,
+			List<Position> capturePositions, List<Position> piecePositions, int longestStreak,Position position) {
+
+		Position p1 = new Position(0, 0);
+		Position p2 = new Position(0, 0);
+
+		p1.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p1) && isThereOpponentPiece(p1) && !ghostPieces[p1.getRow()][p1.getColumn()]) {
+			p2.setValues(position.getRow() + 2, position.getColumn() + 2);
+			if (getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2)) {
+				ghostPieces[p1.getRow()][p1.getColumn()]=true;
+				capturedPieces.add((CheckersPiece)getBoard().piece(p1));
+				capturePositions.add(p1);
+				piecePositions.add(p2);
+				longestStreak+=1;
+				
+				possibleCaptures(ghostPieces,capturedPieces,capturePositions,piecePositions,longestStreak,p2);
+				
+				if (longestStreak>this.longestStreak) {
+					this.capturedPieces = new ArrayList<>();
+					this.capturePositions = new ArrayList<>();
+					this.piecePositions = new ArrayList<>();
+					this.longestStreak=longestStreak;
+					this.capturedPieces.add(capturedPieces);
+					this.capturePositions.add(capturePositions);
+					this.piecePositions.add(piecePositions);
+				} else if (longestStreak==this.longestStreak) {
+					this.capturedPieces.add(capturedPieces);
+					this.capturePositions.add(capturePositions);
+					this.piecePositions.add(piecePositions);
+				}
+				
+				ghostPieces[p1.getRow()][p1.getColumn()]=false;
+				capturedPieces.remove((CheckersPiece)getBoard().piece(p1));
+				capturePositions.remove(p1);
+				piecePositions.remove(p2);
+				longestStreak-=1;
+			}
+		}
+
+		p1.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p1) && isThereOpponentPiece(p1) && !ghostPieces[p1.getRow()][p1.getColumn()]) {
+			p2.setValues(position.getRow() - 2, position.getColumn() + 2);
+			if (getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2)) {
+				ghostPieces[p1.getRow()][p1.getColumn()]=true;
+				capturedPieces.add((CheckersPiece)getBoard().piece(p1));
+				capturePositions.add(p1);
+				piecePositions.add(p2);
+				longestStreak+=1;
+				
+				possibleCaptures(ghostPieces,capturedPieces,capturePositions,piecePositions,longestStreak,p2);
+				
+				if (longestStreak>this.longestStreak) {
+					this.capturedPieces = new ArrayList<>();
+					this.capturePositions = new ArrayList<>();
+					this.piecePositions = new ArrayList<>();
+					this.longestStreak=longestStreak;
+					this.capturedPieces.add(capturedPieces);
+					this.capturePositions.add(capturePositions);
+					this.piecePositions.add(piecePositions);
+				} else if (longestStreak==this.longestStreak) {
+					this.capturedPieces.add(capturedPieces);
+					this.capturePositions.add(capturePositions);
+					this.piecePositions.add(piecePositions);
+				}
+				
+				ghostPieces[p1.getRow()][p1.getColumn()]=false;
+				capturedPieces.remove((CheckersPiece)getBoard().piece(p1));
+				capturePositions.remove(p1);
+				piecePositions.remove(p2);
+				longestStreak-=1;
+			}
+		}
+
+		p1.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p1) && isThereOpponentPiece(p1) && !ghostPieces[p1.getRow()][p1.getColumn()]) {
+			p2.setValues(position.getRow() + 2, position.getColumn() - 2);
+			if (getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2)) {
+				ghostPieces[p1.getRow()][p1.getColumn()]=true;
+				capturedPieces.add((CheckersPiece)getBoard().piece(p1));
+				capturePositions.add(p1);
+				piecePositions.add(p2);
+				longestStreak+=1;
+				
+				possibleCaptures(ghostPieces,capturedPieces,capturePositions,piecePositions,longestStreak,p2);
+				
+				if (longestStreak>this.longestStreak) {
+					this.capturedPieces = new ArrayList<>();
+					this.capturePositions = new ArrayList<>();
+					this.piecePositions = new ArrayList<>();
+					this.longestStreak=longestStreak;
+					this.capturedPieces.add(capturedPieces);
+					this.capturePositions.add(capturePositions);
+					this.piecePositions.add(piecePositions);
+				} else if (longestStreak==this.longestStreak) {
+					this.capturedPieces.add(capturedPieces);
+					this.capturePositions.add(capturePositions);
+					this.piecePositions.add(piecePositions);
+				}
+				
+				ghostPieces[p1.getRow()][p1.getColumn()]=false;
+				capturedPieces.remove((CheckersPiece)getBoard().piece(p1));
+				capturePositions.remove(p1);
+				piecePositions.remove(p2);
+				longestStreak-=1;
+			}
+		}
+
+		p1.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p1) && isThereOpponentPiece(p1) && !ghostPieces[p1.getRow()][p1.getColumn()]) {
+			p2.setValues(position.getRow() - 2, position.getColumn() - 2);
+			if (getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2)) {
+				ghostPieces[p1.getRow()][p1.getColumn()]=true;
+				capturedPieces.add((CheckersPiece)getBoard().piece(p1));
+				capturePositions.add(p1);
+				piecePositions.add(p2);
+				longestStreak+=1;
+				
+				possibleCaptures(ghostPieces,capturedPieces,capturePositions,piecePositions,longestStreak,p2);
+				
+				if (longestStreak>this.longestStreak) {
+					this.capturedPieces = new ArrayList<>();
+					this.capturePositions = new ArrayList<>();
+					this.piecePositions = new ArrayList<>();
+					this.longestStreak=longestStreak;
+					this.capturedPieces.add(capturedPieces);
+					this.capturePositions.add(capturePositions);
+					this.piecePositions.add(piecePositions);
+				} else if (longestStreak==this.longestStreak) {
+					this.capturedPieces.add(capturedPieces);
+					this.capturePositions.add(capturePositions);
+					this.piecePositions.add(piecePositions);
+				}
+				
+				ghostPieces[p1.getRow()][p1.getColumn()]=false;
+				capturedPieces.remove((CheckersPiece)getBoard().piece(p1));
+				capturePositions.remove(p1);
+				piecePositions.remove(p2);
+				longestStreak-=1;
+			}
+		}
+
+		
+	}
+
 	@Override
 	public boolean isThereACapture() {
 
-		Position p = new Position(0,0);
-		
+		Position p = new Position(0, 0);
+
 		p.setValues(position.getRow() + 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			p.setValues(position.getRow() + 2, position.getColumn() + 2);
@@ -55,7 +204,7 @@ public class Man extends CheckersPiece{
 				return true;
 			}
 		}
-		
+
 		p.setValues(position.getRow() - 1, position.getColumn() + 1);
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			p.setValues(position.getRow() - 2, position.getColumn() + 2);
@@ -63,7 +212,7 @@ public class Man extends CheckersPiece{
 				return true;
 			}
 		}
-		
+
 		p.setValues(position.getRow() + 1, position.getColumn() - 1);
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			p.setValues(position.getRow() + 2, position.getColumn() - 2);
@@ -71,7 +220,7 @@ public class Man extends CheckersPiece{
 				return true;
 			}
 		}
-		
+
 		p.setValues(position.getRow() - 1, position.getColumn() - 1);
 		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			p.setValues(position.getRow() - 2, position.getColumn() - 2);
@@ -82,5 +231,5 @@ public class Man extends CheckersPiece{
 
 		return false;
 	}
-	
+
 }
